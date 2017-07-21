5b49323e40eb901e2bf68643af799e1ee8784f86
ramut <sivam7781@gmail.com>
2017-07-21 11:55:24 +0530

    mvn commit 1

---
+++ src/main/java/ramutati/App.java
@@ -0,0 +1,13 @@
+package ramutati;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
